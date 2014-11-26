02390de922cae2c1cd1c7fe6937869622cb1d9e7
Bryan Hughes <bryan@theoreticalideations.com>
2014-11-26 00:57:49 -0800

    Initial import of files

---
+++ index.js
@@ -0,0 +1,344 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2014 Bryan Hughes <bryan@theoreticalideations.com> (http://theoreticalideations.com)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+var fs = require('fs');
+var revision;
+var pins;
+
+// Version lookup info obtained from:
+// http://raspberryalphaomega.org.uk/2013/02/06/automatic-raspberry-pi-board-revision-detection-model-a-b1-and-b2/
+var BOARD_REVISIONS = {
+  0x00: 'Unknown',
+  0x02: 'B1',
+  0x03: 'B1',
+  0x04: 'B2',
+  0x05: 'B2',
+  0x06: 'B2',
+  0x07: 'A',
+  0x08: 'A',
+  0x09: 'A',
+  0x0D: 'B2',
+  0x0E: 'B2',
+  0x0F: 'B2',
+  0x10: 'BPLUS',
+  0x12: 'APLUS'
+};
+
+var B1 = {
+  1: [],
+  2: [],
+  3: [
+    'GPIO0',
+    'SDA'
+  ],
+  4: [],
+  5: [
+    'GPIO1',
+    'SCL'
+  ],
+  6: [],
+  7: [
+    'GPIO4'
+  ],
+  8: [
+    'GPIO14',
+    'TXD'
+  ],
+  9: [],
+  10: [
+    'GPIO15',
+    'RXD'
+  ],
+  11: [
+    'GPIO17'
+  ],
+  12: [
+    'GPIO18'
+  ],
+  13: [
+    'GPIO21'
+  ],
+  14: [],
+  15: [
+    'GPIO22'
+  ],
+  16: [
+    'GPIO23'
+  ],
+  17: [],
+  18: [
+    'GPIO24'
+  ],
+  19: [
+    'GPIO10',
+    'MOSI'
+  ],
+  20: [],
+  21: [
+    'GPIO9',
+    'MISO'
+  ],
+  22: [
+    'GPIO25'
+  ],
+  23: [
+    'GPIO11',
+    'SCLK'
+  ],
+  24: [
+    'GPIO8',
+    'CIE'
+  ],
+  25: [],
+  26: [
+    'GPIO7',
+    'CIE'
+  ]
+};
+
+var B2 = {
+  1: [],
+  2: [],
+  3: [
+    'GPIO2',
+    'SDA'
+  ],
+  4: [],
+  5: [
+    'GPIO3',
+    'SCL'
+  ],
+  6: [],
+  7: [
+    'GPIO4'
+  ],
+  8: [
+    'GPIO14',
+    'TXD'
+  ],
+  9: [],
+  10: [
+    'GPIO15',
+    'RXD'
+  ],
+  11: [
+    'GPIO17'
+  ],
+  12: [
+    'GPIO18'
+  ],
+  13: [
+    'GPIO27'
+  ],
+  14: [],
+  15: [
+    'GPIO22'
+  ],
+  16: [
+    'GPIO23'
+  ],
+  17: [],
+  18: [
+    'GPIO24'
+  ],
+  19: [
+    'GPIO10',
+    'MOSI'
+  ],
+  20: [],
+  21: [
+    'GPIO9',
+    'MISO'
+  ],
+  22: [
+    'GPIO25'
+  ],
+  23: [
+    'GPIO11',
+    'SCLK'
+  ],
+  24: [
+    'GPIO8',
+    'CIE'
+  ],
+  25: [],
+  26: [
+    'GPIO7',
+    'CIE'
+  ]
+};
+
+var BPLUS = {
+  1: [],
+  2: [],
+  3: [
+    'GPIO0',
+    'SDA'
+  ],
+  4: [],
+  5: [
+    'GPIO1',
+    'SCL'
+  ],
+  6: [],
+  7: [
+    'GPIO4'
+  ],
+  8: [
+    'GPIO14',
+    'TXD'
+  ],
+  9: [],
+  10: [
+    'GPIO15',
+    'RXD'
+  ],
+  11: [
+    'GPIO17'
+  ],
+  12: [
+    'GPIO18'
+  ],
+  13: [
+    'GPIO21'
+  ],
+  14: [],
+  15: [
+    'GPIO22'
+  ],
+  16: [
+    'GPIO23'
+  ],
+  17: [],
+  18: [
+    'GPIO24'
+  ],
+  19: [
+    'GPIO10',
+    'MOSI'
+  ],
+  20: [],
+  21: [
+    'GPIO9',
+    'MISO'
+  ],
+  22: [
+    'GPIO25'
+  ],
+  23: [
+    'GPIO11',
+    'SCLK'
+  ],
+  24: [
+    'GPIO8',
+    'CIE'
+  ],
+  25: [],
+  26: [
+    'GPIO7',
+    'CIE'
+  ],
+  27: [],
+  28: [],
+  29: [
+    'GPIO5'
+  ],
+  30: [],
+  31: [
+    'GPIO6'
+  ],
+  32: [
+    'GPIO12'
+  ],
+  33: [
+    'GPIO13'
+  ],
+  34: [],
+  35: [
+    'GPIO12'
+  ],
+  36: [],
+  37: [
+    'GPIO26'
+  ],
+  38: [
+    'GPIO20'
+  ],
+  39: [],
+  40: [
+    'GPIO21'
+  ]
+};
+
+module.exports.getBoardVersion = function(cb) {
+  if (revision) {
+    process.nextTick(cb.bind(undefined, null, revision));
+    return;
+  }
+  fs.readFile('/proc/cpuinfo', function(err, buf) {
+    if (err) {
+      cb(err);
+    } else {
+      var rev = buf.toString().match(/Revision\s*:\s*(.*)/);
+      rev = parseInt(rev && rev[1] || '0', 16);
+      revision = BOARD_REVISIONS[rev];
+
+      switch(revision) {
+        case 'A':
+          // Information is scarce, going to have to reverse-engineer the
+          // schematics to figure out pinouts, if any actually cares about the A
+          throw new Error('Rev A boards are not yet supported.');
+          break;
+        case 'B1':
+          pins = B1;
+          break;
+        case 'B2':
+          pins = B2;
+          break;
+        case 'APLUS':
+        case 'BPLUS':
+          pins = BPLUS;
+          break;
+        default:
+          throw new Error('Unknown board revision ' + revision);
+      }
+
+      cb(null, revision);
+    }
+  });
+};
+
+module.exports.getBoardPins = function(cb) {
+  if (!pins) {
+    module.exports.getBoardVersion(function (err) {
+      if (err) {
+        cb(err);
+        return;
+      }
+      cb(null, pins);
+    });
+  } else {
+    process.nextTick(cb.bind(undefined, null, pins));
+  }
+};
